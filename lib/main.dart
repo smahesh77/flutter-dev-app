bf812e6b73a3347bcae7a6aa6f19779bac38f9ae
Dominic Prince <dominicprince2003@gmail.com>
2023-03-20 20:34:52 +0530

    waiting for op

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:carbon_footprint_calc/car2.dart';
 import 'package:carbon_footprint_calc/pie.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'home.dart';
 import 'calc.dart';
 import 'sign.dart';
@@ -10,7 +11,9 @@ import 'loading.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
